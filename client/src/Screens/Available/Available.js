335e3bed44a35ec87a54e5553949942129ed89c5
James Yeates <jimibue@gmail.com>
2020-07-07 12:29:44 -0600

    prelunch

--- client/src/Screens/Available/Available.js
+++ client/src/Screens/Available/Available.js
@@ -1,13 +1,41 @@
 import React, { useState, useEffect } from "react";
 import AvailableTable from "./AvailableTable";
+import axios from "axios";
 
 export default function Available() {
   const [agents, setAgents] = useState([]);
 
+  formatData(data){
+    []
+
+  }
+
+  async function getAgentData() {
+    let res = await axios.get("/api/properties");
+
+     //TODO:  Format data
+    formatData(res.data)
+    debugger;
+
+    // {beds: 4
+    //   baths: 8
+    // city: "Sandy"
+    // email: "xuan@rohan.com"
+    // first_name: "Earlean"
+    // id: 1
+    // last_name: "O'Hara"
+    // price: 620756
+    // sold: false
+    // sq_ft: 5335
+    // street: "228 Abernathy Burg"
+    // zip: "31593-7711"}
+  }
+
   useEffect(() => {
     //TODO: setup axios call
+    getAgentData();
 
-    //TODO:  Fix data
+  
 
     setAgents([
       {
