8f8c00b722ffb8ff3c1ceea7b534e711b03e729c
James Yeates <jimibue@gmail.com>
2020-07-08 10:48:28 -0600

    rails api endpoint for cities complete

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -3,4 +3,9 @@ class Api::PropertiesController < ApplicationController
     render json: Property.available
     # render json: Property.all
   end
+
+  def city
+    render json: Property.by_city(params[:city])
+    # render json: Property.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
   namespace :api do
     get "properties", to: "properties#index"
+    get "cities/:city", to: "properties#city"
   end
 end
