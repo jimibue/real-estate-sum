03e3ca300786c0c42e082561de67c0b6076c7468
James Yeates <jimibue@gmail.com>
2020-07-08 11:00:34 -0600

    basic setup for citi es page

---
+++ client/src/Screens/Cities/Cities.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Cities extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>cities here</h1>
+      </div>
+    );
+  }
+}
